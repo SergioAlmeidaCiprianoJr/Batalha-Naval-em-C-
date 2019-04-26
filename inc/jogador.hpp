6bf1334cfab6c8c64b3ef46392672c4b5ef14f06
Sergio Junior <sergiosacj@hotmail.com.br>
2019-04-26 11:06:02 -0300

    Criando classe 'jogador'

---
+++ inc/jogador.hpp
@@ -0,0 +1,28 @@
+#ifndef JOGADOR_HPP
+#define JOGADOR_HPP
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Jogador: public Mapa {
+
+private:
+	string nome;
+	
+public:
+    Jogador(); 
+    Jogador(string nome);   
+    ~Jogador();
+    
+	string get_nome();
+	void set_nome(string nome);
+    bool condicaoVitoria();
+    
+};
+
+#endif
---
+++ src/jogador.cpp
@@ -0,0 +1,39 @@
+#include "mapa.hpp"
+#include "embarcacoes.hpp"
+#include "portaAviao.hpp"
+#include "submarino.hpp"
+#include "canoa.hpp"
+#include "jogador.hpp"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+Jogador::Jogador(){
+	nome = "";
+    set_embarcacoesRestantes(0);
+    tamanhoMapa(13, 13);
+}
+Jogador::Jogador(string nome){
+	nome = nome;
+    set_embarcacoesRestantes(0);
+    tamanhoMapa(13, 13);
+}
+Jogador::~Jogador(){
+}
+
+string Jogador::get_nome(){
+	return nome;
+}
+void Jogador::set_nome(string nome){
+	this->nome = nome;
+}
+bool Jogador::condicaoVitoria(){
+	if(get_embarcacoesRestantes() == 0){
+		cout << "Parabéns " << nome.c_str() << ", você GANHOUUUUUU!!!\n";
+		return true;
+	}
+	else return false;
+}
